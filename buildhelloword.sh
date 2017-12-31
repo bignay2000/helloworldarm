871bd982fcadc37cf4ab529a1ef99ef1fcd74b20
Ryan Naylor <bignay2000@hotmail.com>
2017-12-31 15:32:18 -0500

    wip

---
+++ buildhelloword.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#This script runs on X86 but will generate an ARM compiled helloword linux executible
+
+docker run --rm -v "$PWD":/usr/src/helloworldlinuxarm -w /usr/src/helloworldlinuxarm -e GOOS=linux -e GOARCH=arm golang go build -v
