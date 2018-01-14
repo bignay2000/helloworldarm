5b5fdaffff8993e3b1ac0d5b7ade09a3c009d935
Ryan Naylor <bignay2000@hotmail.com>
2018-01-13 20:40:22 -0500

    corrected comment

--- buildhelloword.sh
+++ buildhelloword.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-#This script runs on X86 but will generate an ARM compiled helloword linux executible "helloworldlinuxarm"
+#This script runs on X86 but will generate an ARM 32 compiled helloword linux executible "helloworldlinuxarm" that is compatible with the Raspberry Pi Zero W
 
 docker run --rm -v "$PWD":/usr/src/helloworldlinuxarm -w /usr/src/helloworldlinuxarm -e GOOS=linux -e GOARCH=arm golang go build -v
